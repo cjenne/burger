26c89bdc18d48ce41deca0ee583ab9c6a03a2bfa
Duane Dibbley <44147755+cjenne@users.noreply.github.com>
2019-02-17 21:38:17 -0700

    minor changes for page reload after update

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -16,7 +16,7 @@ $(function() {
       function() {
         console.log("changed devour to", newdevour);
         // Reload the page to get the updated list
-        loburgerion.reload();
+        location.reload();
       }
     );
   });
@@ -38,23 +38,23 @@ $(function() {
       function() {
         console.log("created new burger");
         // Reload the page to get the updated list
-        loburgerion.reload();
+        location.reload();
       }
     );
   });
 
-  $(".delete-burger").on("click", function(event) {
-    var id = $(this).data("id");
+  // $(".delete-burger").on("click", function(event) {
+  //   var id = $(this).data("id");
 
-    // Send the DELETE request.
-    $.ajax("/api/burgers/" + id, {
-      type: "DELETE"
-    }).then(
-      function() {
-        console.log("deleted burger", id);
-        // Reload the page to get the updated list
-        loburgerion.reload();
-      }
-    );
-  });
+  //   // Send the DELETE request.
+  //   $.ajax("/api/burgers/" + id, {
+  //     type: "DELETE"
+  //   }).then(
+  //     function() {
+  //       console.log("deleted burger", id);
+  //       // Reload the page to get the updated list
+  //       location.reload();
+  //     }
+  //   );
+  // });
 });
